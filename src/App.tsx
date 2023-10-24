9787f08a90e7b321580f46bce12e9a17276602f8
bigperson <daein7076@gmail.com>
2023-10-24 19:09:44 +0900

    3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="hello" />
-    </div>
+    <Container>
+      <H1>hello</H1>
+    </Container>
   );
 }
 
