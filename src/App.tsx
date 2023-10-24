3a7809560bd8fd3f4c0f073db471388c9b7b64d5
bigperson <daein7076@gmail.com>
2023-10-24 18:28:57 +0900

    props and optional props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Title>Hello</Title>
-      </Box>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" borderColor="yellow" />
+      <Circle bgColor="tomato" text="hello" />
+    </div>
   );
 }
 
