fb91bea5f1d09b11c8ff1a3e3431b50cd5285b55
bigperson <daein7076@gmail.com>
2023-10-29 22:10:27 +0900

    make gh-pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Coin from "./routes/Coin";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Coins />} />
         {/* nested router 를 사용할 때는 * 을 항상 입력해주자!! */}
